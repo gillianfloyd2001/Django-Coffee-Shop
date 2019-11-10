6e99d3166d6963e9e3ecef9fbeb7b994223e2251
Student <student@donation.local>
2019-11-10 11:56:00 -0600

    creating models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Coffee(models.Model):
+    name = models.TextField()
+    price = models.FloatField()
+
+
+class Transaction(models.Model):
+    time = models.DateTimeField()
+    item = models.ForeignKey(Coffee, on_delete=models.PROTECT)
+    pre_tax = models.FloatField()
+    tax = models.FloatField()
